e521145fbb42dd08d5a522d2519b9874fa23d843
Loh Siu Yin <siuyin@beyondbroadcast.com>
2018-07-08 12:24:23 +0000

    Added markMilestone reactor.

--- cmd/markMilestone/main.go
+++ cmd/markMilestone/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"sync"
 	"time"
 
 	stan "github.com/nats-io/go-nats-streaming"
@@ -14,7 +13,7 @@ import (
 // 10 OMIT
 const (
 	clusterID = "test-cluster"
-	clientID  = "newForOld-newID"
+	clientID  = "newForOld-markMilestone"
 	svrURL    = "nats://192.168.99.100:4222"
 )
 
@@ -24,52 +23,39 @@ type Payload struct {
 	OldID     string //     o123
 	NewID     string //     nABC
 }
+type Milestone struct {
+	TimeStamp time.Time
+	Event     string // eg. MilestoneMarked
+	N         int
+}
 
 // 20 OMIT
 func main() {
-	fmt.Println("newForOld newID Query Handler", time.Now())
-	// 30 OMIT
+	const milestoneInterval = 2
+	fmt.Println("newForOld Mark Milestone Command Handler", time.Now())
 	sc, _ := stan.Connect(clusterID, clientID, stan.NatsURL(svrURL),
 		stan.SetConnectionLostHandler(func(_ stan.Conn, reason error) {
 			log.Fatalf("Connection lost, reason: %v", reason)
 		}))
 	defer sc.Close()
-	// 31 OMIT
-	idAccess := sync.Mutex{}
-	idMap := make(map[string]string)
-	newData := make(chan bool)
-	// 32 OMIT
-	// 33 OMIT
-	startOpt := stan.DeliverAllAvailable()
 	subject := "newForOld"
-	sc.Subscribe(subject, func(msg *stan.Msg) {
+	var n int
+	// 30 OMIT
+	startOpt := stan.DeliverAllAvailable()
+	sc.Subscribe(subject, func(msg *stan.Msg) { // HL
 		if !bytes.Contains(msg.Data, []byte(`"IDMapped"`)) {
 			return
 		}
 		pl := Payload{}
 		json.Unmarshal(msg.Data, &pl)
-		idAccess.Lock()
-		idMap[pl.OldID] = pl.NewID
-		idAccess.Unlock()
-		newData <- true
-	}, startOpt)
-	// 34 OMIT
-	listIDs(idMap, newData)
-	select {} // wait forever
-	// 40 OMIT
-}
-
-// 50 OMIT
-func listIDs(ids map[string]string, ch <-chan bool) {
-	go func() {
-		for {
-			<-ch // wait for new data
-			fmt.Println("New Listing", time.Now())
-			for k, v := range ids {
-				fmt.Printf("%s %s\n", k, v)
-			}
+		n++
+		if n%milestoneInterval == 0 {
+			ms := Milestone{pl.TimeStamp, "MilestoneMarked", n}
+			milestone, _ := json.Marshal(ms)
+			sc.Publish(subject, milestone) // HL
+			fmt.Printf("Milestone: %d marked at %v\n", n, pl.TimeStamp)
 		}
-	}()
+	}, startOpt)
+	// 40 OMIT
+	select {} // wait forever
 }
-
-// 60 OMIT
