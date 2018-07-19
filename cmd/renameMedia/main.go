6f893e818baf79a4527823a9f3c428a971f2e1ec
Loh Siu Yin <siuyin@beyondbroadcast.com>
2018-07-19 12:56:45 +0000

    Added renameMedia example.

--- cmd/renameMedia/main.go
+++ cmd/renameMedia/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 	"time"
 
 	stan "github.com/nats-io/go-nats-streaming"
@@ -14,58 +13,53 @@ import (
 
 const (
 	clusterID = "test-cluster"
-	clientID  = "newForOld-newID"
+	clientID  = "newForOld-renameMedia"
 	svrURL    = "nats://192.168.99.100:4222"
 )
 
 type Payload struct {
 	TimeStamp time.Time
-	Event     string // eg. IDMapped
+	Event     string // eg. RenameRequested / Renamed / RenameFailed
 	OldID     string //     o123
 	NewID     string //     nABC
 }
 
 func main() {
-	fmt.Println("newForOld newID Query Handler", time.Now())
+	fmt.Println("newForOld renameMedia reactor")
 	sc, _ := stan.Connect(clusterID, clientID, stan.NatsURL(svrURL),
 		stan.SetConnectionLostHandler(func(_ stan.Conn, reason error) {
 			log.Fatalf("Connection lost, reason: %v", reason)
 		}))
 	defer sc.Close()
 
-	idAccess := sync.Mutex{}
-	idMap := make(map[string]string)
 	// 10 OMIT
 	startOpt := stan.DeliverAllAvailable()
 	subject := "newForOld"
 	sc.Subscribe(subject, func(msg *stan.Msg) {
-		if !bytes.Contains(msg.Data, []byte(`"IDMapped"`)) {
+		if !bytes.Contains(msg.Data, []byte(`"RenameRequested"`)) { // HL
 			return
 		}
 		pl := Payload{}
 		json.Unmarshal(msg.Data, &pl)
-		idAccess.Lock()
-		idMap[pl.OldID] = pl.NewID
-		idAccess.Unlock()
+		// Actually do renaming here ...
+		fmt.Printf("!! renamed: %q -> %q\n", pl.OldID, pl.NewID)
+		pl.Event = "Renamed" // HL
+		plBytes, _ := json.Marshal(pl)
+		sc.Publish(subject, plBytes) // HL
+		fmt.Printf("%s event created\n", plBytes)
 	}, startOpt)
 	// 20 OMIT
 	// 30 OMIT
-	http.HandleFunc("/id", func(w http.ResponseWriter, req *http.Request) {
-		id := req.FormValue("id")
-		idAccess.Lock()
-		fmt.Fprintf(w, "new ID for:%q is :%q\n", id, idMap[id])
-		idAccess.Unlock()
+	http.HandleFunc("/rename", func(w http.ResponseWriter, req *http.Request) {
+		oID := req.FormValue("oID")
+		nID := req.FormValue("nID")
+		subject := "newForOld"
+		pl := Payload{time.Now(), "RenameRequested", oID, nID}
+		plBytes, _ := json.Marshal(pl)
+		sc.Publish(subject, plBytes) // HL
+		fmt.Fprintf(w, "%s event created\n", plBytes)
 	})
 	// 40 OMIT
-	// 50 OMIT
-	http.HandleFunc("/ids", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Listing all IDs. Time:%v\n", time.Now().Format("15:04:05.000"))
-		idAccess.Lock()
-		for k, v := range idMap {
-			fmt.Fprintf(w, "%s %s\n", k, v)
-		}
-		idAccess.Unlock()
-	})
-	// 60 OMIT
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
